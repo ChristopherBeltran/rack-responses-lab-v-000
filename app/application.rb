e2759869bf4777c808ead1ce92072bab5552d9d6
Christopher Beltran <christod.beltran@gmail.com>
2018-11-06 04:12:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,17 +2,28 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
- 
-    time1 = Time.new
-    binding.pry
-    if time1 > 12
-      resp.write "Good Afternoon!"
-    elsif
-    time1 < 12
-    resp.write "Good Morning!"
-  end
+ now = Time.now
+  today = Date.today.to_time
+
+  morning = today.beginning_of_day
+  noon = today.noon
+  evening = today.change( hour: 17 )
+  night = today.change( hour: 20 )
+  tomorrow = today.tomorrow
+
+  if (morning..noon).cover? now
+    resp.write 'Good Morning!'
+  elsif (noon..evening).cover? now
+    resp.write 'Good Afternoon!'
+    #if time1 > 12
+      #resp.write "Good Afternoon!"
+   # elsif
+   # time1 < 12
+    #resp.write "Good Morning!"
+ # end
  
     resp.finish
   end
+end 
  
 end
