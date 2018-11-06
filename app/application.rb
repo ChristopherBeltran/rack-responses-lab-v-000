995d951421feae046223a2022b8d2721e58de26e
Christopher Beltran <christod.beltran@gmail.com>
2018-11-06 04:10:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Application
  
   def call(env)
     resp = Rack::Response.new
  
     time1 = Time.new
-    
+    binding.pry
     if time1 > 12
       resp.write "Good Afternoon!"
     elsif
