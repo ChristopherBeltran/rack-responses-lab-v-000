b1015133a3bacf42f9605280d4c10ceabc01ff0c
Christopher Beltran <christod.beltran@gmail.com>
2018-11-06 04:05:19 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+ 
+    time1 = Time.new
+    
+    if time1 > 12
+      resp.write "Good Afternoon!"
+    elsif
+    time1 < 12
+    resp.write "Good Morning!"
+  end
+ 
+    resp.finish
+  end
+ 
+end
